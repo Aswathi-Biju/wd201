dfae8c885421c9ebb4db1b9c3460cd8bf1d81875
Aswathi Biju <aswathibiju8118@gmail.com>
2025-06-09 21:30:24 +0530

    index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,38 +1,40 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const minimist = require('minimist')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const minimist = require('minimist');
 
-const args = minimist(process.argv.slice(2))
-const port = args.port || 3000
+// Parse command line arguments
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(`Request URL: ${req.url}`)
-  if (req.url === '/' || req.url === '/home') {
-    serve('home.html', res)
-  } else if (req.url === '/project') {
-    serve('project.html', res)
-  } else if (req.url === '/registration') {
-    serve('registration.html', res)
-  } else {
-    res.writeHead(404)
-    res.end('404 Not Found')
-  }
-})
+  console.log(`Request for ${req.url}`);
 
-function serve(filename, res) {
-  const filePath = path.join(__dirname, filename)
+  if (req.url === '/' || req.url === '/home') {
+    serveFile('home.html', res);
+  } else if (req.url === '/project') {
+    serveFile('project.html', res);
+  } else if (req.url === '/registration') {
+    serveFile('registration.html', res);
+  } else {
+    res.statusCode = 404;
+    res.end('404 Not Found');
+  }
+});
+
+function serveFile(filename, res) {
+  const filePath = path.join(__dirname, filename);
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(500)
-      res.end('500 Internal Server Error')
+      res.statusCode = 500;
+      res.end('Internal Server Error');
     } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.end(data)
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
     }
-  })
+  });
 }
 
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
